68c28b2f29209b3285f4efaec77c0f2aeff1c8b4
rahulkalantre <rahulkalantre97@gmail.com>
2023-08-31 12:14:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
 function App() {
+
+  const [num, setNum] = useState(0);
+
+  // useEffect(() => {
+  //   if (num === 0) {
+  //     setNum(5 + Math.random() * 50);
+  //   }
+  // }, [num]);
+  // console.log(num);
+
+  useLayoutEffect(() => {
+    if (num === 0) {
+      setNum(5 + Math.random() * 50);
+    }
+  }, [num]);
+  console.log(num);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> { num } </h2>
+      <button onClick = { () => setNum(0) }> Check </button>
     </div>
   );
 }
